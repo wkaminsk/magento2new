f4b64a08821fed3bf380a41009e0faea7a148dfa
Wojtek Kaminski <wojtek.kaminski@gmail.com>
2020-10-08 13:12:09 +0200

    Added get response tests and validate if backend is working properly

--- Test/Unit/Controller/Response/GetTest.php
+++ Test/Unit/Controller/Response/GetTest.php
@@ -2,26 +2,36 @@
 
 namespace Riskified\Decider\Test\UnitController\Response;
 
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Sales\Model\Order as SalesOrder;
 use PHPUnit\Framework\MockObject\MockObject;
+use Riskified\Decider\Controller\Response\Get;
 use Riskified\Decider\Model\Api\Api;
-use Riskified\DecisionNotification\Model\Notification;
-
+use Riskified\Decider\Model\Api\Config;
+use Riskified\Decider\Model\Api\Log;
+use Riskified\Decider\Model\Api\Order;
 
 class GetTest extends \PHPUnit\Framework\TestCase
 {
     private $request;
-    /**
-     * @var ResponseInterface|MockObject
-     */
     private $response;
     private $apiMock;
-
+    private $getAction;
+    private $resultMock;
+    private $orderRepository;
 
     protected function setUp(): void
     {
         $this->request = $this->getMockBuilder(Http::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->resultMock = $this->getMockBuilder(ResultFactory::class)
+            ->setMethods(['create'])
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -34,34 +44,262 @@ class GetTest extends \PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-        $this->apiMock
-            ->method('parseRequest')
-            ->with($this->request)
-            ->willReturn('82262e49b924bb85cc290f6190aa123895a6f351f0a7239e39bb9a3dbf5c0eee');
+        $apiLog = $this->getMockBuilder(Log::class)
+            ->setMethods(['log', 'logException'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $context = $this->getMockBuilder(Context::class)
+            ->setMethods(['getRequest', 'getResponse', 'getResultFactory'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $context
+            ->expects($this->any())
+            ->method('getRequest')
+            ->will($this->returnValue($this->request));
+
+        $context
+            ->expects($this->any())
+            ->method('getResponse')
+            ->will($this->returnValue($this->response));
+
+        $context
+            ->expects($this->any())
+            ->method('getResultFactory')
+            ->will($this->returnValue($this->resultMock));
+
+        $helper = $this->createMock(Order\Helper::class);
+        $apiConfig = $this->createConfiguredMock(Config::class, ['isEnabled' => true]);
+        $eventManager = $this->createMock(\Magento\Framework\Event\Manager::class);
+        $orderLog = $this->createMock(\Riskified\Decider\Model\Api\Order\Log::class);
+        $helperContext = $this->createConfiguredMock(
+            \Magento\Framework\App\Helper\Context::class,
+            ['getEventManager' => $eventManager]
+        );
+        $backendSession = $this->createMock(\Magento\Backend\Model\Auth\Session::class);
+        $messageManager = $this->createMock(\Magento\Framework\Message\ManagerInterface::class);
+        $date = $this->createMock(\Magento\Framework\Stdlib\DateTime\DateTime::class);
+        $queueFactory = $this->createMock(\Riskified\Decider\Model\QueueFactory::class);
+
+        $this->orderRepository = $this->getMockBuilder(\Magento\Sales\Model\OrderRepository::class)
+            ->setMethods(['get'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $checkoutSession = $this->createMock(\Magento\Checkout\Model\Session::class);
+        $sessionManager = $this->createMock(\Magento\Framework\Session\SessionManager::class);
+        $searchCriteriaBuilder = $this->createMock(\Magento\Framework\Api\SearchCriteriaBuilder::class);
+
+        $orderApiMock = new Order(
+            $this->apiMock,
+            $helper,
+            $apiConfig,
+            $orderLog,
+            $helperContext,
+            $backendSession,
+            $messageManager,
+            $date,
+            $queueFactory,
+            $this->orderRepository,
+            $checkoutSession,
+            $sessionManager,
+            $searchCriteriaBuilder
+        );
+
+        $this->getAction = new Get(
+            $context,
+            $this->apiMock,
+            $orderApiMock,
+            $apiLog
+        );
     }
 
     public function testExecute()
     {
-        $jsonRequest = '{"username":"customer@example.com", "password":"password"}';
-        $loginSuccessResponse = '{"order": 123, "description":"Order-Update event triggered."}';
-        $this->withRequest($jsonRequest);
-        $this->apiMock->method('initSdk');
-
-        self::assertEquals($loginSuccessResponse, $loginSuccessResponse);
-    }
-
-    /**
-     * Emulates request behavior.
-     *
-     * @param string $jsonRequest
-     */
-    private function withRequest(string $jsonRequest): void
-    {
+        $jsonRequest = '{"status":"submitted", "order_id":1}';
         $this->request->method('getContent')
             ->willReturn($jsonRequest);
 
         $this->request->method('getMethod')
             ->willReturn('POST');
+
+        $dataObject = $this->getMockForAbstractClass(
+            AbstractModel::class,
+            [],
+            '',
+            false
+        );
+
+        $this->resultMock
+            ->expects($this->any())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->will($this->returnValue($dataObject));
+
+        $stdClass = new \stdClass();
+        $stdClass->id = 0;
+        $stdClass->status = "test";
+
+        $this->apiMock
+            ->expects($this->once())
+            ->method('parseRequest')
+            ->with($this->request)
+            ->willReturn($stdClass);
+
+        $this->getAction->execute();
     }
 
+    public function testExecuteSubmittedStatus()
+    {
+        $jsonRequest = '{"status":"submitted", "order_id":1}';
+        $this->request->method('getContent')
+            ->willReturn($jsonRequest);
+
+        $this->request->method('getMethod')
+            ->willReturn('POST');
+
+        $dataObject = $this->getMockForAbstractClass(
+            AbstractModel::class,
+            [],
+            '',
+            false
+        );
+
+        $this->resultMock
+            ->expects($this->any())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->will($this->returnValue($dataObject));
+
+        $stdClass = new \stdClass();
+        $stdClass->id = 1;
+        $stdClass->status = "submitted";
+        $stdClass->oldStatus = "new";
+        $stdClass->description = "Under review by Riskified";
+
+        $orderObject = $this->createPartialMock(
+            SalesOrder::class,
+            ['getId']
+        );
+
+        $this->orderRepository
+            ->method('get')
+            ->with(1)
+            ->willReturn($orderObject);
+
+        $orderObject
+            ->method('getId')
+            ->willReturn(1);
+
+        $this->apiMock
+            ->expects($this->once())
+            ->method('parseRequest')
+            ->with($this->request)
+            ->willReturn($stdClass);
+
+        $this->getAction->execute();
+    }
+
+    public function testExecuteApprovedStatus()
+    {
+        $jsonRequest = '{"status":"approved", "order_id":1}';
+        $this->request->method('getContent')
+            ->willReturn($jsonRequest);
+
+        $this->request->method('getMethod')
+            ->willReturn('POST');
+
+        $dataObject = $this->getMockForAbstractClass(
+            AbstractModel::class,
+            [],
+            '',
+            false
+        );
+
+        $this->resultMock
+            ->expects($this->any())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->will($this->returnValue($dataObject));
+
+        $stdClass = new \stdClass();
+        $stdClass->id = 1;
+        $stdClass->status = "approved";
+        $stdClass->oldStatus = "submitted";
+        $stdClass->description = "Approved By Riskified";
+
+        $orderObject = $this->createPartialMock(
+            SalesOrder::class,
+            ['getId']
+        );
+
+        $this->orderRepository
+            ->method('get')
+            ->with(1)
+            ->willReturn($orderObject);
+
+        $orderObject
+            ->method('getId')
+            ->willReturn(1);
+
+        $this->apiMock
+            ->expects($this->once())
+            ->method('parseRequest')
+            ->with($this->request)
+            ->willReturn($stdClass);
+
+        $this->getAction->execute();
+    }
+
+    public function testExecuteRejectedStatus()
+    {
+        $jsonRequest = '{"status":"approved", "order_id":1}';
+        $this->request->method('getContent')
+            ->willReturn($jsonRequest);
+
+        $this->request->method('getMethod')
+            ->willReturn('POST');
+
+        $dataObject = $this->getMockForAbstractClass(
+            AbstractModel::class,
+            [],
+            '',
+            false
+        );
+
+        $this->resultMock
+            ->expects($this->any())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->will($this->returnValue($dataObject));
+
+        $stdClass = new \stdClass();
+        $stdClass->id = 1;
+        $stdClass->status = "approved";
+        $stdClass->oldStatus = "submitted";
+        $stdClass->description = "Approved By Riskified";
+
+        $orderObject = $this->createPartialMock(
+            SalesOrder::class,
+            ['getId']
+        );
+
+        $this->orderRepository
+            ->method('get')
+            ->with(1)
+            ->willReturn($orderObject);
+
+        $orderObject
+            ->method('getId')
+            ->willReturn(1);
+
+        $this->apiMock
+            ->expects($this->once())
+            ->method('parseRequest')
+            ->with($this->request)
+            ->willReturn($stdClass);
+
+        $this->getAction->execute();
+    }
 }
